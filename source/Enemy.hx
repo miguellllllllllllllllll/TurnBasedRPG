8531fd7d35fe11abc7e25bd67dc2ba8e29e49c87
Miguel <152848745+Miguellllllllllllllllll@users.noreply.github.com>
2024-10-23 16:38:11 +0200

    ok

---
+++ source/Enemy.hx
@@ -0,0 +1,164 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.math.FlxPoint;
+import flixel.math.FlxVelocity;
+import flixel.sound.FlxSound;
+
+using flixel.util.FlxSpriteUtil;
+
+enum EnemyType
+{
+	REGULAR;
+	BOSS;
+}
+
+class Enemy extends FlxSprite
+{
+	static inline var WALK_SPEED:Float = 40;
+	static inline var CHASE_SPEED:Float = 70;
+
+	public var type:EnemyType;
+    var brain:FSM;
+    var idleTimer:Float;
+    var moveDirection:Float;
+    public var seesPlayer:Bool;
+    public var playerPosition:FlxPoint;
+	var stepSound:FlxSound;
+
+	public function new(x:Float, y:Float, type:EnemyType)
+	{
+		super(x, y);
+		this.type = type;
+		var graphic = if (type == BOSS) AssetPaths.frieza__png else AssetPaths.cooler__png;
+		loadGraphic(graphic, true, 35, 44);
+		setFacingFlip(LEFT, false, false);
+		setFacingFlip(RIGHT, true, false);
+		animation.add("d_idle", [0]);
+		animation.add("lr_idle", [3]);
+		animation.add("u_idle", [6]);
+		animation.add("d_walk", [0, 1, 0, 2], 6);
+		animation.add("lr_walk", [3, 4, 3, 5], 6);
+		animation.add("u_walk", [6, 7, 6, 8], 6);
+		drag.x = drag.y = 10;
+		 // Hitbox
+        //Set.size formula: x/2, y/2
+        //offset.set formula: x/4, y/2
+		setSize(17, 22);
+		offset.x = 12;
+		offset.y = 22;
+        brain = new FSM(idle);
+        idleTimer = 0;
+        playerPosition = FlxPoint.get();
+	}
+    function idle(elapsed:Float)
+        {
+            if (seesPlayer)
+            {  
+                brain.activeState = chase;
+            }
+            else if (idleTimer <= 0)
+            {
+                // 95% chance to move
+                if (FlxG.random.bool(95))
+                {
+                    moveDirection = FlxG.random.int(0, 8) * 45;
+        
+                    velocity.setPolarDegrees(WALK_SPEED, moveDirection);
+                }
+                else
+                {
+                    moveDirection = -1;
+                    velocity.x = velocity.y = 0;
+                }
+                idleTimer = FlxG.random.int(1, 4);
+            }
+            else
+                idleTimer -= elapsed;
+            
+        }
+        
+        function chase(elapsed:Float)
+        {
+            if (!seesPlayer)
+            {
+                brain.activeState = idle;
+            }
+            else
+            {
+                FlxVelocity.moveTowardsPoint(this, playerPosition, CHASE_SPEED);
+            }
+        }
+
+		public function changeType(type:EnemyType)
+			{
+				if (this.type != type)
+				{
+					this.type = type;
+					var graphic = if (type == BOSS) AssetPaths.frieza__png else AssetPaths.cooler__png;
+					loadGraphic(graphic, true, 35, 44);
+				}
+			}
+
+			
+	override public function update(elapsed:Float)
+	{
+
+		if (this.isFlickering())
+			return;
+
+		var action = "idle";
+		if (velocity.x != 0 || velocity.y != 0)
+		{   
+			action = "walk";
+			if (Math.abs(velocity.x) > Math.abs(velocity.y))
+			{
+				if (velocity.x < 0)
+					facing = LEFT;
+				else
+					facing = RIGHT;
+			}
+			else
+			{
+				if (velocity.y < 0)
+					facing = UP;
+				else
+					facing = DOWN;
+			}
+		}
+
+		switch (facing)
+		{
+			case LEFT, RIGHT:
+				animation.play("lr_" + action);
+
+			case UP:
+				animation.play("u_" + action);
+
+			case DOWN:
+				animation.play("d_" + action);
+
+			case _:
+		}
+        brain.update(elapsed);
+		super.update(elapsed);
+	}
+}
+
+
+class FSM
+{
+	public var activeState:Float->Void;
+
+	public function new(initialState:Float->Void)
+	{
+		activeState = initialState;
+	}
+
+	public function update(elapsed:Float)
+	{
+		activeState(elapsed);
+	}
+    
+}
---
+++ source/HUD.hx
@@ -0,0 +1,49 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+
+using flixel.util.FlxSpriteUtil;
+
+class HUD extends FlxTypedGroup<FlxSprite>
+{
+	var background:FlxSprite;
+	var healthCounter:FlxText;
+	var moneyCounter:FlxText;
+	var healthIcon:FlxSprite;
+	var moneyIcon:FlxSprite;
+    var charIcon:FlxSprite;
+
+	public function new()
+	{
+		super();
+		background = new FlxSprite().makeGraphic(FlxG.width, 20, FlxColor.BLACK);
+		background.drawRect(0, 19, FlxG.width, 1, FlxColor.WHITE);
+		healthCounter = new FlxText(16, 2, 0, "3 / 3", 8);
+		healthCounter.setBorderStyle(SHADOW, FlxColor.GRAY, 1, 1);
+		moneyCounter = new FlxText(0, 2, 0, "0", 8);
+		moneyCounter.setBorderStyle(SHADOW, FlxColor.GRAY, 1, 1);
+		healthIcon = new FlxSprite(4, healthCounter.y + (healthCounter.height/2)  - 7, AssetPaths.health__png);
+		moneyIcon = new FlxSprite(FlxG.width - 20, moneyCounter.y + (moneyCounter.height/2) - 8, AssetPaths.coin__png);
+        charIcon = new FlxSprite(5,25,AssetPaths.gokuportrait__png);
+		moneyCounter.alignment = RIGHT;
+		moneyCounter.x = moneyIcon.x - moneyCounter.width;
+		add(background);
+		add(healthIcon);
+		add(moneyIcon);
+		add(healthCounter);
+        add(charIcon);
+		add(moneyCounter);
+		forEach(function(sprite) sprite.scrollFactor.set(0, 0));
+	}
+
+	public function updateHUD(health:Int, money:Int)
+	{
+		healthCounter.text = health + " / 3";
+		moneyCounter.text = Std.string(money);
+		moneyCounter.x = moneyIcon.x - moneyCounter.width - 7 ;
+	}
+}
